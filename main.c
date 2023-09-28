b7905bdedd1c31f2309d23e72103d343baffd406
bangkarlsen <jhpe04@gmail.com>
2023-09-28 21:19:53 +0200

    Add serial debug logging
    For reasons way above my head "%d" does not work in the format string
    for kprintf?! It keeps printing 0 when used. But, when I use snprintf to
    first generate the string and then log it using kprintf all is well.
    
    Wont pursue this further for now even though it creates a small overhead
    when logging.

--- main.c
+++ main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
+#include <clib/debug_protos.h>
 
 extern Start();
 
+#define MSG_LENGTH 32
+char text[MSG_LENGTH];
+#define SERIAL_LOG(msg) kprintf("%s", msg);
+#define SERIAL_LOG_FMT(fmt, var) snprintf(text, MSG_LENGTH, fmt, var); kprintf(text);
+
 int main() {
 
-        printf("\nFind your tr0ffel and romkugler - here we go!..--~~~>>0\n\n(kom saa demo)\n\n\n\n");
+        printf("\nFind your tr0ffel and romkugler - here we go!..--~~~>>0\n\n(kom saa demo)\n\n");
+        SERIAL_LOG("\n<Begin debug>\n")
 
         Start();
 
+        SERIAL_LOG("\n<EOD>\n")
         return 0;
 }
 
-void calculate( __reg("a0") unsigned char *screen, __reg("d0") step )
+void calculate( __reg("a0") unsigned char *screen, __reg("d0") int step )
 {
         const int width=320;
 
         int x = 160 + step%20;
         int y = 80 + step%34;
+
+        SERIAL_LOG_FMT("step=%d\n", step)
 
         screen[x+y*width] = step%256;
 }
